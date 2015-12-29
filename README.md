14fe9ca09e733c3be12b9e401a48f804326e4f33
ChenXiao <chenxiao07@baidu.com>
2015-12-29 16:10:06 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# edp-url-embed-processor
+# edp-build-url-embed
 
 Embed URL's as base64 strings inside your stylesheets using edp build
+
+
+## Install
+
+```shell
+npm install edp-build-url-embed --save-dev
+```
+
+## Usage
+
+Add code in `edp-build-config.js`
+
+```javascript
+var UrlEmbededProcessor = require('edp-build-url-embed');
+var urlEmbed = new UrlEmbededProcessor();
+```
+
+
+### Options
+
+#### inclusive
+
+Type: `Boolean`
+
+Default: `false`
+
+Specifies the mode of embedding.
+* `true` (inclusive) means that you have to manually mark each URL that needs to be embedded using the `/* embed */` comment.
+* `false` (exclusive) means that every URL is embedded, except those that are marked with `/* noembed */` comment.
+
+
+### Excluding URLs manually (when inclusive: false)
+
+```css
+.exclude-me {
+    background-image: url('exclude_me.png'); /* noembed */
+}
+```
+
+### Including URLs manually (when inclusive: true)
+
+```css
+.include-me {
+    background-image: url('include_me.png'); /* embed */
+}
+```
+
+### When URLs are in the middle of CSS property
+
+```css
+.include-me1 {
+    background: transparent url('include_me.png') /* embed */ center center no-repeat;
+}
+
+.include-me2 {
+    background-image: -webkit-image-set(url('include_me1.png') /* embed */ 1x, url('include_me2.png') /* embed */ 2x);
+}
+```
---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * @file UrlEmbededProcessor
+ * @author cxtom(cxtom2010@gmail.com)
+ */
+
+/* globals AbstractProcessor */
+
+var util = require('util');
+var path = require('path');
+var mime = require('mime');
+var assert = require('assert');
+
+
+var BASE_URL_REGEX = 'url\\(["\']?([^"\'\\(\\)]+?)["\']?\\)[};,!\\s]';
+var EXCLUSIVE_URL_REGEX = BASE_URL_REGEX + '(?!\\s*?\\/\\*\\s*?noembed\\s*?\\*\\/)';
+var INCLUSIVE_URL_REGEX = BASE_URL_REGEX + '\\s*?\\/\\*\\s*?embed\\s*?\\*\\/';
+var EMBEDDABLE_URL_REGEX = /^data:/;
+var REMOTE_URL_REGEX = /^(http|https):/;
+
+function UrlEmbededProcessor(options) {
+    AbstractProcessor.call(this, options);
+}
+
+util.inherits(UrlEmbededProcessor, AbstractProcessor);
+
+
+UrlEmbededProcessor.DEFAULT_OPTIONS = {
+    name: 'UrlEmbededProcessor',
+    files: ['*.styl', '*.css', '*.less'],
+    extensions: ['.jpg', '.png'],
+
+    inclusive: true
+};
+
+
+/**
+ * 构建处理
+ *
+ * @param {FileInfo} file 文件信息对象
+ * @param {ProcessContext} processContext 构建环境对象
+ * @param {Function} callback 处理完成回调函数
+ */
+UrlEmbededProcessor.prototype.process = function (file, processContext, callback) {
+
+    var fileContent = file.data;
+    var filePath = file.path;
+
+    var basePath = path.resolve(__dirname, '..');
+
+    var urlRegex = new RegExp(this.inclusive ? INCLUSIVE_URL_REGEX : EXCLUSIVE_URL_REGEX, 'g');
+
+    var me = this;
+
+    var data = fileContent.replace(urlRegex, function ($0, $1) {
+
+        var url = $1;
+
+        if (url.match(EMBEDDABLE_URL_REGEX)
+            || url.match(REMOTE_URL_REGEX)
+            || me.extensions.indexOf(path.extname(url)) < 0) {
+            return url;
+        }
+
+        url = path.resolve(path.dirname(filePath), url);
+        url = path.relative(basePath, url);
+
+        var resource = processContext.getFileByPath(url);
+
+        // console.log(resource);
+
+        assert(resource, 'embed resource not exist:' + url);
+
+        var base64Content = resource.data.toString('base64');
+        var mimeType = mime.lookup(url);
+
+        var dataUri = '("data:' + mimeType + ';base64,' + base64Content + '")';
+
+        return dataUri;
+
+    });
+
+    file.setData(data);
+
+    callback();
+
+};
+
+module.exports = UrlEmbededProcessor;
